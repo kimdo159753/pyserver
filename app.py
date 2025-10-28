35707f07932396c06cc7d3478adc5208f33db06b
ddobba <kimdo159753@gmail.com>
2025-10-28 17:33:54 +0900

    simple paige trans

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/write')
+def write():
+    return render_template('write.html')
+
+@app.route('/save-memo', methods=['POST'])
+def save_memo():
+    pass
+
 if __name__ == "__main__":
     app.run()
